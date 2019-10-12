f777f9bc7e0dde697f1469cc87e70851fcf77d22
Mohammed Noori <kmanoorib@gmail.com>
2019-10-12 02:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ document.querySelector("main#main").remove();
 let newHeader = document.createElement("h1");
 
 newHeader.id = "victory"
-newHeader.innerHTML = "is the champion!";
+newHeader.innerHTML = "YOUR-NAME is the champion!";
 newHeader.className = "victory";
